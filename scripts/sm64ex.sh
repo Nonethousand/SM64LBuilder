713244a2da1386e8a5fc535616987c6ec0e78b78
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-09 15:07:40 -0700

    Update and rename test.sh to sm64ex.sh

---
+++ scripts/sm64ex.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/sm64pc/sm64ex.git && cd sm64ex && echo "Please copy your baserom.us.z64 to ~/SM64LBuilder/repos/sm64ex/"
--- scripts/test.sh
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/sm64pc/sm64ex.git && cd sm64ex

905a0cf9b6b040c706bf57a5dc2b47a200386d07
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-10 10:06:50 -0700

    Update sm64ex.sh

--- scripts/sm64ex.sh
+++ scripts/sm64ex.sh
@@ -1,1 +1,11 @@
-git clone https://github.com/sm64pc/sm64ex.git && cd sm64ex && echo "Please copy your baserom.us.z64 to ~/SM64LBuilder/repos/sm64ex/"
+cd ~/SM64LBuilder/repos
+git clone https://github.com/sm64pc/sm64ex.git
+if [ -e ~/baserom.us.z64 ]; then
+cp ~/baserom.us.z64 sm64ex
+cd sm64ex
+echo "Starting compilation of sm64ex... (build flags and jobs are not available right now.) "
+make
+echo "sm64ex compiled!"
+else
+echo "baserom.us.z64 not found in home directory. Please put your baserom into your home directory."
+fi

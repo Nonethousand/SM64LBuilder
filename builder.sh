596b5406c653ae85984a307ab4d9c28c36c6487e
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-10 10:44:43 -0700

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,18 +1,17 @@
 if [ $1 -eq custom_repo ]; then
 cd repos
 git clone $2
-if [ -e ~/baserom.us.z64 ]; then
-cp ~/baserom.us.z64 $3
-cd $3
-echo "Starting compilation of $3... (build flags and jobs are not available right now.) "
-make
-echo $3 "compiled!"
-else
-cd ~/SM64LBuilder/repos
-rm $3
-echo "baserom.us.z64 not found in home directory. Please put your baserom into your home directory."
-fi
-done
+  if [ -e ~/baserom.us.z64 ]; then
+  cp ~/baserom.us.z64 $3
+  cd $3
+  echo "Starting compilation of $3... (build flags and jobs are not available right now.) "
+  make
+  echo $3 "compiled!"
+  else
+  cd ~/SM64LBuilder/repos
+  rm $3
+  echo "baserom.us.z64 not found in home directory. Please put your baserom into your home directory."
+  fi
 else
 cd scripts
 if [ -e $1.sh ]; then

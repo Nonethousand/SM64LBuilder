f4b38ccacac4da4675697c7a8c178f8880bf91ea
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-10 09:50:23 -0700

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,7 +1,8 @@
-if [ -e scripts/$1 .sh ]; then
+cd scripts
+if [ -e $1.sh ]; then
 echo $1 "found! Compiling..."
-chmod 755 scripts/$1 .sh
-sh $1 .sh
+chmod 755 $1.sh
+sh $1.sh
 else
 echo $1 "not found!"
 fi

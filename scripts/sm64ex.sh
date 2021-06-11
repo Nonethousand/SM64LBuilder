65b38ad1bfb394764be5b217dc3595024854b1a8
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 11:02:58 -0700

    Update sm64ex.sh

--- scripts/sm64ex.sh
+++ scripts/sm64ex.sh
@@ -11,7 +11,11 @@ read number
 for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
 fi
 echo "Starting compilation of sm64ex... (build flags and jobs are not available right now.) "
+if [ $1 -- "--jobs" ]; then
+make $2
+else
 make
+fin
 echo "sm64ex compiled!"
 else
 cd ~/SM64LBuilder/repos

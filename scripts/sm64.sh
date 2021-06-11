a3ef99bad504d039cded0e28cf94881e406ca903
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 11:05:03 -0700

    Update sm64.sh

--- scripts/sm64.sh
+++ scripts/sm64.sh
@@ -10,8 +10,12 @@ echo "How many?"
 read number
 for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
 fi
-echo "Starting compilation of sm64... (build flags and jobs are not available right now.) "
+echo "Starting compilation of sm64... (build flags are not available right now.) "
+if [ $1 -- "--jobs" ]; then
+make $2
+else
 make
+fi
 echo "sm64 compiled!"
 else
 cd ~/SM64LBuilder/repos

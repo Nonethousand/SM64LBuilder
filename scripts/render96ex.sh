f8fc9aa40e33f83323923e33750646743da6c64e
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 09:32:24 -0700

    Update render96ex.sh

--- scripts/render96ex.sh
+++ scripts/render96ex.sh
@@ -4,6 +4,13 @@ git clone --single-branch --branch alpha https://github.com/Render96/Render96ex.
 if [ -e ~/baserom.us.z64 ]; then
 cp ~/baserom.us.z64 Render96ex
 cd Render96ex
+echo "Would you like patches? [y/n]"
+read answer
+if [ $answer = "y" ]; then
+echo "How many?"
+read number
+for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
+fi
 echo "Starting compilation of Render96ex... (build flags and jobs are not available right now.) "
 make
 echo "Render96ex compiled!"

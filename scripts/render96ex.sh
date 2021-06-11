bffb7b5643497df1540d618f8dfdbd5ddf261fd6
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 11:05:26 -0700

    Update render96ex.sh

--- scripts/render96ex.sh
+++ scripts/render96ex.sh
@@ -11,8 +11,12 @@ echo "How many?"
 read number
 for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
 fi
-echo "Starting compilation of Render96ex... (build flags and jobs are not available right now.) "
+echo "Starting compilation of Render96ex... (build flags are not available right now.) "
+if [ $1 -- "--jobs" ]; then
+make $2
+else
 make
+fi
 echo "Render96ex compiled!"
 else
 cd ~/SM64LBuilder/repos

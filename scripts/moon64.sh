b2f03024b97b9f3338020d4a73a482bd3db4065c
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 15:36:41 -0700

    Update moon64.sh

--- scripts/moon64.sh
+++ scripts/moon64.sh
@@ -1,14 +1,14 @@
 cd ~/SM64LBuilder/repos
 git clone https://github.com/KiritoDv/Moon64.git
-if [ -e ~/baserom.us.z64 ]; then
-cp ~/baserom.us.z64 Moon64
+echo "Please select your baserom.us.z64."
+cp $(zenity --file-selection --file-filter='z64 ROMS (z64) | *.z64' --title="Select your baserom.us.z64 ROM") Moon64
 cd Moon64
 echo "Would you like patches? [y/n]"
 read answer
 if [ $answer = "y" ]; then
 echo "How many?"
 read number
-for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
+for i in {1..$number}; do echo "Chose a file" && git apply $(zenity --file-selection --file-filter='patch files (patch) | *.patch' --title="Select your patch file"); done
 fi
 echo "Starting compilation of Moon64... (build flags are not available right now.) "
 if [ $1 -- "--jobs" ]; then
@@ -17,8 +17,4 @@ else
 make
 fi
 echo "Moon64 compiled!"
-else
-cd ~/SM64LBuilder/repos
-rm Moon64
-echo "baserom.us.z64 not found in home directory. Please put your baserom into your home directory."
-fi
+

6be4e395bc4d12f26572042cb40747ca2310994c
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 11:05:50 -0700

    Update moon64.sh

--- scripts/moon64.sh
+++ scripts/moon64.sh
@@ -10,8 +10,12 @@ echo "How many?"
 read number
 for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
 fi
-echo "Starting compilation of Moon64... (build flags and jobs are not available right now.) "
+echo "Starting compilation of Moon64... (build flags are not available right now.) "
+if [ $1 -- "--jobs" ]; then
+make $2
+else
 make
+fi
 echo "Moon64 compiled!"
 else
 cd ~/SM64LBuilder/repos

6c0d5df84b6321a9fbf2df5db9b8b8efe17038f1
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 09:22:07 -0700

    Update sm64ex.sh

--- scripts/sm64ex.sh
+++ scripts/sm64ex.sh
@@ -4,8 +4,8 @@ if [ -e ~/baserom.us.z64 ]; then
 cp ~/baserom.us.z64 sm64ex
 cd sm64ex
 echo "Would you like patches? [y/n]"
-read patches
-if [ $patches == "y" ]; then
+read answer
+if [ $answer == "y" ]; then
 echo "How many?"
 read number
 for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done

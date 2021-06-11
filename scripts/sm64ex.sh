0e8c6bef5814130091af0438290b5e5f07449b37
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 09:14:51 -0700

    Update sm64ex.sh

--- scripts/sm64ex.sh
+++ scripts/sm64ex.sh
@@ -3,6 +3,13 @@ git clone https://github.com/sm64pc/sm64ex.git
 if [ -e ~/baserom.us.z64 ]; then
 cp ~/baserom.us.z64 sm64ex
 cd sm64ex
+echo "Would you like patches? [y/n]"
+read patches
+if [ $patches == "y" ]; then
+echo "How many?"
+read number
+for i in {1..$number}; do echo "Enter your patch location" && read location && git apply $location; done
+fin
 echo "Starting compilation of sm64ex... (build flags and jobs are not available right now.) "
 make
 echo "sm64ex compiled!"

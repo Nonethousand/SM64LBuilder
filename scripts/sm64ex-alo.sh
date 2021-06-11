9984f9af4965bc77e26c2418a6a2f19c613e9311
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 16:18:18 -0700

    Create sm64ex-alo.sh

---
+++ scripts/sm64ex-alo.sh
@@ -0,0 +1,19 @@
+cd ~/SM64LBuilder/repos
+https://github.com/AloXado320/sm64ex-alo.git
+echo "Please select your baserom.us.z64."
+cp $(zenity --file-selection --file-filter='z64 ROMS (z64) | *.z64' --title="Select your baserom.us.z64 ROM") sm64ex-alo
+cd sm64ex-alo
+echo "Would you like patches? [y/n]"
+read answer
+if [ $answer = "y" ]; then
+echo "How many?"
+read number
+for i in {1..$number}; do echo "Chose a file" && git apply $(zenity --file-selection --file-filter='patch files (patch) | *.patch' --title="Select your patch file"); done
+fi
+echo "Starting compilation of sm64ex-alo..."
+if [ $1 -- "--jobs" ]; then
+make $(zenity --forms --title="Build Flags" --add-entry=Flags) $2
+else
+make $(zenity --forms --title="Build Flags" --add-entry=Flags)
+fi
+echo "sm64ex-alo compiled!"

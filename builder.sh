aa84d207e3b3c5c053e98b3c7288f91f0c70e242
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-11 20:50:15 -0700

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -27,7 +27,7 @@ echo $2 "is not a directory!"
 fi
 else
 if [ $1 == "--help" ]; then
-echo "Commands: --help = see this help message, --library = see the built in repos, --custom-repo = build with a custom repo, --remove-builder = remove SM64LBuilder, --remove-repo = remove a repo"
+echo "Commands: --help = see this help message, --library = see the built in repos, --custom-repo = build with a custom repo, --remove-builder = remove SM64LBuilder, --remove-repo = remove a repo, --jobs = build with jobs, --update = get instructions on how to update"
 else
 if [ $1 == "--library" ]; then
 cd ~/SM64LBuilder/scripts

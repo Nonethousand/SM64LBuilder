22e5db1a453acf10be121cc2663401138d95efe9
HiImBlahh <78574005+HiImBlahh@users.noreply.github.com>
2021-06-13 15:05:21 -0700

    added build flag gui

--- scripts/sm64ex-coop.sh
+++ scripts/sm64ex-coop.sh
@@ -11,9 +11,10 @@ read number
 for i in {1..$number}; do echo "Chose a file" && git apply $(zenity --file-selection --file-filter='patch files (patch) | *.patch' --title="Select your patch file"); done
 fi
 echo "Starting compilation of sm64ex-coop..."
+IN=$(zenity --list --checklist --title "Build Flags" --text "Flags" --column "" --column "Options" True VERSION=us False VERSION=eu False VERSION=jp False VERSION=sh False TARGET_BITS=32 True TARGET_BITS=64 False TARGET_RPI=1 False TARGET_WEB=1 False WINDOWS_BUILD=1 False OSX_BUILD=1)
 if [ $1 == "--jobs" ]; then
-make $(zenity --forms --title="Build Flags" --add-entry=Flags) $2
+make $(echo $IN | tr "|" " ") $2
 else
-make $(zenity --forms --title="Build Flags" --add-entry=Flags)
+make $(echo $IN | tr "|" " ")
 fi
 echo "sm64ex-coop compiled!"
--- scripts/sm64ex.sh
+++ scripts/sm64ex.sh
@@ -1,6 +1,6 @@
 cd ~/SM64LBuilder/repos
 git clone https://github.com/sm64pc/sm64ex.git
-echo "Please select your baserom.us.z64."
+echo "Please select your baserom"
 cp $(zenity --file-selection --file-filter='z64 ROMS (z64) | *.z64' --title="Select your baserom.us.z64 ROM") sm64ex
 cd sm64ex
 echo "Would you like patches? [y/n]"
@@ -11,9 +11,10 @@ read number
 for i in {1..$number}; do echo "Chose a file" && git apply $(zenity --file-selection --file-filter='patch files (patch) | *.patch' --title="Select your patch file"); done
 fi
 echo "Starting compilation of sm64ex..."
+IN=$(zenity --list --checklist --title "Build Flags" --text "Flags" --column "" --column "Options" True VERSION=us False VERSION=eu False VERSION=jp False VERSION=sh False TARGET_BITS=32 True TARGET_BITS=64 False TARGET_RPI=1 False TARGET_WEB=1 False WINDOWS_BUILD=1 False OSX_BUILD=1)
 if [ $1 == "--jobs" ]; then
-make $(zenity --forms --title="Build Flags" --add-entry=Flags) $2
+make $(echo $IN | tr "|" " ") $2
 else
-make $(zenity --forms --title="Build Flags" --add-entry=Flags)
+make $(echo $IN | tr "|" " ")
 fi
 echo "sm64ex compiled!"
